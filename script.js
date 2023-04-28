b9a367597819bedb38cee30a67dc2d4b458bf39e
shahinxasmemmed <sahinxasmemmedov5@gmail.com>
2023-04-28 11:33:35 +0400

    final

---
+++ script.js
@@ -0,0 +1,25 @@
+// const data = [
+//     { id: '000000001', date: '13.02.2022', amount: '10 AZN' },
+//     { id: '000000002', date: '13.02.2022', amount: '153 AZN' },
+//     { id: '000000003', date: '13.02.2022', amount: '18 AZN' },
+//     { id: '000000004', date: '13.02.2022', amount: '15 AZN' },
+// ]
+
+
+
+// data.forEach(item => {
+//     receiptBody.innerHTML += `<tr  class="receipt" data-key=${item.id}><td><i class="fa-regular fa-newspaper"></i>${item.id}</td><td>${item.date}</td><td>${item.amount}</td></tr>`;
+    
+// });
+
+// const receipt = document.querySelectorAll('.receipt');
+//     receipt.forEach(item => {
+//         item.addEventListener('click', e => {
+//             data.forEach(data => {
+//                 if (item.dataset.key === data.id) {
+//                     console.log(data)
+//                 }
+//             })
+            
+//         })
+//     })
